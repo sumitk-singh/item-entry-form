ea48932200563e73f413f2575f56d98dc27c72a0
SUMIT SINGH <sumit.email.dev@gmail.com>
2022-10-01 03:27:18 +0000

    added form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <Form />
+    
     </div>
   );
 }
--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -2,9 +2,22 @@ import "./Form.css";
 
 const Form = () => {
   return <>
-  
-  
-  
+    <h2>Item Entry Form</h2>
+    <hr />
+    <div className="form-box">
+      <label htmlFor="">Item Name</label>
+      <input type="text" />
+
+      <label htmlFor="">Qty</label>
+      <input type="number" />
+
+      <label htmlFor="">Price</label>
+      <input type="number" />
+
+      <button>Enter</button>
+
+      <hr />
+    </div>
   </>;
 };
 
